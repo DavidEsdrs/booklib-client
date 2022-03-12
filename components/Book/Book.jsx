1afd44637b55c2340d9b9a505780d6be07094baf
DavidEsdrs <carpina1983@gmail.com>
2022-03-12 17:07:11 -0300

    Layout component rendered

--- components/Book/Book.jsx
+++ components/Book/Book.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from "./Book.module.scss";
 
 const Book = ({ id, title, author, excerpt, published_at }) => {
     return (
-        <div>
-            
+        <div className={styles.container}>
+            <h1>{title}</h1>
         </div>
     )
 }
