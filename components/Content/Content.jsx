6d323b68bdb597dd7259f21ddc5fe9f0ca45ee54
DavidEsdrs <carpina1983@gmail.com>
2022-03-13 14:01:22 -0300

    AddBookContextProvider is now in the Content component

--- components/Content/Content.jsx
+++ components/Content/Content.jsx
@@ -3,17 +3,20 @@ import Book from "../Book/Book";
 import styles from "./Content.module.scss";
 import NewBookWrapper from "../NewBookWrapper/NewBookWrapper";
 import { BooksContext } from "../BooksContext/BooksContext.jsx";
+import AddBookContextProvider, { AddBookContext } from "../AddBookContext/AddBookContext";
 
 const Content = () => {
-    const { state } = useContext(BooksContext);
+    const { store } = useContext(BooksContext);
 
     return (
         <div className={styles.container}>
-            <NewBookWrapper />
+            <AddBookContextProvider>
+                <NewBookWrapper />
+            </AddBookContextProvider>
 
             <main className={styles.main}>
                 <ul>
-                    {state.books.map(book => (
+                    {store.books.map(book => (
                         <Book key={book.id} {...book} />
                     ))}
                 </ul>
