1ba5d0153cf1d9c46c62b43f6e2a6831a483c999
DavidEsdrs <carpina1983@gmail.com>
2022-03-13 01:22:15 -0300

    Minor

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Header from "../Header/Header.jsx";
-import { BooksContext } from "../BooksContext/BooksContext.jsx"; 
 
 const Layout = ({ children }) => {
     return (
