c4665551f6156ebb362cdad10f840c3781291e6f
DavidEsdrs <carpina1983@gmail.com>
2022-03-13 02:23:23 -0300

    Add header component and its styles

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from "./Header.module.scss";
 
 const Header = () => {
     return (
-        <header>
-            
+        <header className={styles.container}>
+            <h1>Booklib</h1>
         </header>
     )
 }
